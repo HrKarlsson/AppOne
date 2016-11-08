a30c5da9a5767501fd413f2bdf21b918215c9a50
Mario Roskosch <mario.roskosch@assfinet.de>
2016-11-08 06:34:05 +0100

    Firebase läuft

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/platform";
 
+import {OpaqueToken} from '@angular/core';
+export const FIREBASE: OpaqueToken = new OpaqueToken('firebase');
+
 import { AppComponent } from "./app.component";
 
 @NgModule({
--- app/main.ts
+++ app/main.ts
@@ -2,4 +2,54 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app.module";
 
+
+import firebase = require("nativescript-plugin-firebase");
+
+
+//firebase.addValueEventListener((result: any) => {
+//}, "/example");
+
+
+firebase.init({
+  persist: true
+}).then(
+  function (instance) {
+    console.log("firebase.init done");
+
+    firebase.login({
+      type: firebase.LoginType.PASSWORD,
+      email: 'mario.roskosch@assfinet.de',
+      password: 'Keines:78m'
+    }).then(
+      function (result) {
+        // the result object has these properties: uid, provider, expiresAtUnixEpochSeconds, profileImageURL, token
+        console.log(JSON.stringify(result));
+
+
+        firebase.setValue(
+          '/companies',
+          { foo: 'bar' }
+        );
+
+
+      },
+      function (errorMessage) {
+        console.log("error " + errorMessage);
+      }
+      );
+
+
+
+
+
+  },
+  function (error) {
+    console.log("firebase.init error: " + error);
+  }
+  );
+
+
+
+
+
 platformNativeScriptDynamic().bootstrapModule(AppModule);
